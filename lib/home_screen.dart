e0a9d8ea8e2255f801041f3b1a3dd7d1b07cec65
ahirlog <rhahir2015@gmail.com>
2025-03-12 07:00:00 +0530

    Form validation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_notes/video_picker_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,20 +25,60 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //  Image picker
-            button(
-              context: context,
-              text: 'Video picker',
-              onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                      const VideoPickerScreen())),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _emailController,
+                      showCursor: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
+                      onChanged: (value) {
+                        email = value;
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Enter email' : null;
+                      },
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: _passwordController,
+                      showCursor: true,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
+                      onChanged: (value) {
+                        password = value;
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Enter password' : null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              //  Image picker
+              button(
+                  context: context,
+                  text: 'Login',
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      log('Validate');
+                    }
+                  }),
+            ],
+          ),
         ),
       ),
     );
