973d2e193aeb06df0d58c62d4fb151e420277571
ahir <rhahir2015@gmail.com>
2025-04-16 22:11:49 +0530

    database functionalities

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/db_handler.dart';
+import 'package:flutter_notes/notes.dart' show NotesModel;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +10,105 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DBHelper? dbHelper;
+  late Future<List<NotesModel>> notesList;
+
+  @override
+  void initState() {
+    super.initState();
+    dbHelper = DBHelper();
+    loadData();
+  }
+
+  loadData() async {
+    notesList = dbHelper!.getNotesList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(),
+      appBar: AppBar(
+        title: const Text('SQLite DB'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: notesList,
+                builder: (context, AsyncSnapshot<List<NotesModel>> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: (){
+                            dbHelper!.update(
+                              NotesModel(
+                                id: snapshot.data![index].id!,
+                                title: 'First flitter note',
+                                age: 11,
+                                description: 'let me talk to you tomorrow',
+                                email: 'asf@gmail.com',
+                              ),
+                            );
+                            setState(() {
+                              notesList = dbHelper!.getNotesList();
+                            });
+                          },
+                          child: Dismissible(
+                            direction: DismissDirection.endToStart,
+                            background: Container(
+                              color: Colors.red,
+                              child: const Icon(Icons.delete),
+                            ),
+                            onDismissed: (DismissDirection direction) {
+                              setState(() {
+                                dbHelper!.delete(snapshot.data![index].id!);
+                                notesList = dbHelper!.getNotesList();
+                                snapshot.data!.remove(snapshot.data![index].id!);
+                              });
+                            },
+                            key: ValueKey<int>(snapshot.data![index].id!),
+                            child: Card(
+                              child: ListTile(
+                                title:
+                                    Text(snapshot.data![index].title.toString()),
+                                subtitle: Text(
+                                    snapshot.data![index].description.toString()),
+                                trailing:
+                                    Text(snapshot.data![index].age.toString()),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                }),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          dbHelper!
+              .insert(NotesModel(
+                  title: 'First Note',
+                  age: 22,
+                  description: 'This is my first sql app',
+                  email: 'axiftaj@gmaol.com'))
+              .then((value) {
+            print('data added');
+            setState(() {
+              notesList = dbHelper!.getNotesList();
+            });
+          }).onError((error, stackTrace) {
+            print(error.toString());
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
