980792a20e89e158be426c08491cf08d899abd85
ahir <rhahir2015@gmail.com>
2025-04-16 16:08:36 +0530

    added database

---
+++ lib/db_handler.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_notes/notes.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io' as io;
+
+class DBHelper {
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDataBase();
+
+    return _db;
+  }
+
+  initDataBase() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'notes.db');
+
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+
+    return db;
+  }
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+      "CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL,age INTEGER NOT NULL, description TEXT NOT NULL, email TEXT)",
+    );
+  }
+
+  Future<NotesModel> insert(NotesModel notesModel) async {
+    var dbClient = await db;
+    await dbClient!.insert('notes', notesModel.toMap());
+    return notesModel;
+  }
+}
