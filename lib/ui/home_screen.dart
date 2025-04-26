bb382170fb52ac4818539f04e6df427b77288417
ahir <rhahir2015@gmail.com>
2025-04-26 14:07:20 +0530

    fetch post

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/ui/add_post_screen.dart';
 import 'package:flutter_notes/utils/utils.dart';
@@ -13,6 +15,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ref = FirebaseDatabase.instance.ref('Post');
+
   Future<void> _logout() async {
     await FirebaseAuth.instance.signOut().then((_) {
       Navigator.pushReplacementNamed(context, '/login');
@@ -34,13 +38,56 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: const Center(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context,
               MaterialPageRoute(builder: (context) => const AddPostScreen()));
         },
         child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          // fetch using FirebaseAnimatedList widget
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: ref,
+              defaultChild: const Text('Loading'),
+              itemBuilder: (context, snapshot, animation, index) {
+                return ListTile(
+                  title: Text(snapshot.child('title').value.toString()),
+                  subtitle: Text(snapshot.child('id').value.toString()),
+                );
+              },
+            ),
+          ),
+          // fetch using Stream builder
+          Expanded(
+            child: StreamBuilder(
+              stream: ref.onValue,
+              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                if (!snapshot.hasData) {
+                  return const CircularProgressIndicator();
+                } else {
+                  Map<dynamic, dynamic> map =
+                      snapshot.data!.snapshot.value as dynamic;
+                  List<dynamic> list = [];
+                  list.clear();
+                  list = map.values.toList();
+
+                  return ListView.builder(
+                    itemCount: snapshot.data!.snapshot.children.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(list[index]['title']),
+                        subtitle: Text(list[index]['id']),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
