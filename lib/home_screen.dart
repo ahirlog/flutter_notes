161069994d933a67df2e959d47533f7ea0427732
ahir <rhahir2015@gmail.com>
2025-04-30 15:47:29 +0530

    getX set up and snackbar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,8 +13,24 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('GetX'),
+      ),
       body: const Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.snackbar(
+            'Asif Taj',
+            'Subscribe to my channel',
+            icon: const Icon(Icons.add),
+            onTap: (snap) {},
+            mainButton:
+                TextButton(onPressed: () {}, child: const Text('Click')),
+            backgroundColor: Colors.blue,
+            snackPosition: SnackPosition.BOTTOM,
+          );
+        },
+      ),
     );
   }
 }
