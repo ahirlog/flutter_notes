d7f173c2a13da6b37896b95ba9be182e66164fb1
ahir <rhahir2015@gmail.com>
2025-04-30 12:35:09 +0530

    Hive data add, edit, delete

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,7 +13,49 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: Hive.openBox('asif'),
+            builder: (context, snapshot) {
+              return Column(
+                children: [
+                  ListTile(
+                    title: Text(snapshot.data!.get('age').toString()),
+                    trailing: IconButton(
+                      onPressed: () {
+                        // delete data
+                        snapshot.data!.delete('age');
+
+                        // edit data
+                        // snapshot.data!.put('age', '20');
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.edit),
+                    ), // IconButton
+                  ), // ListTile
+                ],
+              ); // Column
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var box = await Hive.openBox('asif');
+          var box2 = await Hive.openBox('name');
+
+          // add data
+          box.put('age', 25);
+          box.put('details', {
+            'pro': 'developer',
+            'kash': 'sdfsdf',
+          });
+
+          box2.put('youtube', 'Asif Taj Tech');
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
