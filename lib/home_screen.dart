becda7a56576df67aed3e975aa33fb2feb4e5a3e
ahir <rhahir2015@gmail.com>
2025-04-30 08:43:02 +0530

    Shared Preferences

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +9,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String name = '';
+
+  void getData() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    name = sp.getString('name') ?? '';
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(),
+      appBar: AppBar(
+        title: const Text('Shared Preferences'),
+      ),
+      body: Text(name),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          SharedPreferences sp = await SharedPreferences.getInstance();
+          sp.setString('name', 'Asif Taj');
+          getData();
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
