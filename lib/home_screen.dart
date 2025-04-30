57c56ac8f59e61b8439064c643c5b52eec9c0aa5
ahir <rhahir2015@gmail.com>
2025-04-30 15:56:25 +0530

    getX Dialog alert and them change

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,9 +16,44 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('GetX'),
       ),
-      body: const Center(),
+      body: Card(
+        child: ListTile(
+          title: const Text('GetX Bottom Sheet'),
+          subtitle: const Text('GetX dialog alert twith getX'),
+          onTap: () {
+            Get.bottomSheet(
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(30),
+                ), // BoxDecoration
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.light_mode),
+                      title: const Text('Light Theme'),
+                      onTap: () {
+                        Get.changeTheme(ThemeData.light());
+                      },
+                    ), // ListTile
+                    ListTile(
+                      onTap: () {
+                        Get.changeTheme(ThemeData.dark());
+                      },
+                      leading: const Icon(Icons.dark_mode),
+                      title: const Text('Dark Theme'),
+                    ), // ListTile
+                    // ... more ListTiles or widgets ...
+                  ],
+                ), // Column
+              ),
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          // Snack Bar
           Get.snackbar(
             'Asif Taj',
             'Subscribe to my channel',
@@ -28,6 +63,26 @@ class _HomeScreenState extends State<HomeScreen> {
                 TextButton(onPressed: () {}, child: const Text('Click')),
             backgroundColor: Colors.blue,
             snackPosition: SnackPosition.BOTTOM,
+          );
+
+          // Dialog alert
+          Get.defaultDialog(
+            title: 'Delete Chat',
+            titlePadding: const EdgeInsets.only(top: 20),
+            contentPadding: const EdgeInsets.all(20),
+            middleText: 'Are you sure you want to delete this chat?',
+            confirm: TextButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: const Text('Ok'),
+            ),
+            cancel: TextButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: const Text('Cancel'),
+            ),
           );
         },
       ),
