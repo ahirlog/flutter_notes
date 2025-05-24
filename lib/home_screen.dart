57721a6882ab296eab972635a2da9922f02cd81e
ahir <rhahir2015@gmail.com>
2025-05-24 13:37:51 +0530

    send notification from one to another screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/notification_services.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,6 +30,34 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            notificationServices.getDeviceToken().then((value) async {
+              var data = {
+                'to': value.toString(),
+                'priority': 'high',
+                'notification': {
+                  'title': 'Asif',
+                  'body': 'Subscribe to my channel',
+                }
+              };
+
+              await http.post(
+                Uri.parse('https://fcm.googleapis.com/fcm/send'),
+                body: jsonEncode(data),
+                headers: {
+                  'Content-Type': 'application/json; charset=UTF-8',
+                  'Authorization': 'key=AAAAP9pXDFM:APA91bGhBeMCUABE2PXj1...'
+                },
+              );
+            });
+          },
+          child: const Text('Send Notifications'),
+        ),
+      ),
+    );
   }
 }
