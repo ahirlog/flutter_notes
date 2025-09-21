6f0a2ad5c1a0b44c07df3e2a7e5dafd2da0763a9
ahir <rhahir2015@gmail.com>
2025-09-21 22:34:14 +0530

    Image picker bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_notes/Utils/image_picker_utils.dart';
 import 'package:flutter_notes/bloc/counter/counter_bloc.dart';
+import 'package:flutter_notes/bloc/image_picker/image_picker_bloc.dart';
 import 'package:flutter_notes/ui/counter/counter_screen.dart';
 
 void main() {
@@ -10,18 +12,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CounterBloc(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: CounterScreen(),
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => CounterBloc()),
+          BlocProvider(create: (_) => ImagePickerBloc(ImagePickerUtils())),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          themeMode: ThemeMode.dark,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            primarySwatch: Colors.blue,
+          ),
+          home: const CounterScreen(),
+        ));
   }
 }
